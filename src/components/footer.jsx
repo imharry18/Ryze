879100a999afffa58d154d68090deff8286ed76d
Harish Kumar <iharish0018@gmail.com>
2025-09-21 18:17:48 +0530

    Restarted the Project

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import './footer.css'
-function footer(){
-    return(
-        <>
-            <div id="iam">Hey</div>
-        </>
-    )
-}
-export default footer
--- src/components/gridPost.jsx
+++
@@ -1,9 +0,0 @@
-
-function gridPost({link}){
-    return(
-        <>
-            <div  style={{backgroundColor: link, height: "300px", width:"300px"}}></div>
-        </>
-    )
-}
-export default gridPost
